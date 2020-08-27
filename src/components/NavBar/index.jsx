9b8535c49fa9f0bb7c1897db0bdbcd0a6c021d79
toiaa <victoria@geddes.com.ar>
2020-08-26 21:41:56 -0300

    first commit

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NavBar = () =>  <div> 
+        <ul>
+            <li>Episodes</li>
+            <li>Themes</li>
+        </ul>
+    </div>
+    
+export default NavBar;
---
+++ src/components/Welcome/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+//import Button from '@material-ui/core/Button';
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+export default Welcome;
